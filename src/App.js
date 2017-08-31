2210761b9924b368e17d0a7bd40f1bf54385cc87
Conor Doyle <doyle7@hotmail.com>
2017-08-31 04:17:30 +0100

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
+import { HashRouter, Link } from 'react-router';
 import { Layout, Header, Content, Navigation, Drawer } from 'react-mdl';
 import './App.css';
 
 class App extends Component {
   onHandleSubmit = () => {
-    console.log('hi!');
+    console.log('Yo!');
   }
 
   render() {
     return (
       <div className='App'>
-        <Layout fixedHeader>
-          <Header title='Product Hunt PWA'>
-            <Navigation className='hide-on-sm'></Navigation>
-          </Header>
-          <Drawer title='Product Hunt'>
-            <Navigation>
-
-            </Navigation>
-          </Drawer>
-          <Content>
-            Dynamic content here...
-          </Content>
-        </Layout>
+        <HashRouter>
+          <Layout fixedHeader>
+            <Header title='Product Hunt PWA'>
+              <Navigation className='hide-on-sm'>
+                <Link to='/tech'>Tech</Link>
+                <Link to='/games'>Games</Link>
+              </Navigation>
+            </Header>
+            <Drawer title='Product Hunt'>
+              <Navigation>
+                <Link to='/tech'>Tech</Link>
+                <Link to='/games'>Games</Link>
+              </Navigation>
+            </Drawer>
+            <Content>
+              Dynamic content here...
+            </Content>
+          </Layout>
+        </HashRouter>
       </div>
     );
   }
