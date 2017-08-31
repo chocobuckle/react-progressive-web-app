0f64a6c53e2406e1a69d7a7e9084e04601c26b35
Conor Doyle <doyle7@hotmail.com>
2017-08-31 03:20:40 +0100

    Add 'react-mdl' and scaffold basic layout of 'App.js'

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Layout, Header, Content, Navigation, Drawer } from 'react-mdl';
 import './App.css';
 
 class App extends Component {
+  onHandleSubmit = () => {
+    console.log('hi!');
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+        <Layout fixedHeader>
+          <Header title='Product Hunt PWA'>
+            <Navigation className='hide-on-sm'></Navigation>
+          </Header>
+          <Drawer title='Product Hunt'>
+            <Navigation>
+
+            </Navigation>
+          </Drawer>
+          <Content>
+            Dynamic content here...
+          </Content>
+        </Layout>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './serviceWorkerDev';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
