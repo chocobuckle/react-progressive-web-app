116c481e9dadcec7211aa3c93c5e967af8a3c13d
Conor Doyle <doyle7@hotmail.com>
2017-08-31 10:41:56 +0100

    Remove unnessary 'console.log' command

--- src/serviceWorkerDev.js
+++ src/serviceWorkerDev.js
@@ -1,7 +1,5 @@
 export default function regiserServiceWorker() {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register(`${process.env.PUBLIC_URL}sw.js`).then(register => {
-      console.log('Service worker successfully registered: ', register);
-    });
+    navigator.serviceWorker.register(`${process.env.PUBLIC_URL}sw.js`);
   }
 }
