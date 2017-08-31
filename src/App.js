cded17d8c80db48da89e6069ce739cca3b3d6914
Conor Doyle <doyle7@hotmail.com>
2017-08-31 14:12:07 +0100

    Add 'Posts' component and pass 'category' down as props.
    Delete 'Tech' and 'Games' components. We'll just have one 'Posts' component and pass the category down as props.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import { HashRouter, Link, Match, Redirect } from 'react-router';
 import { Layout, Header, Content, Navigation, Drawer } from 'react-mdl';
 import './App.css';
-import Tech from './Tech';
-import Games from './Games';
+import Posts from './Posts';
 
 class App extends Component {
   closeDrawer = () => {
@@ -18,19 +17,25 @@ class App extends Component {
             <Header title='Product Hunt PWA'>
               <Navigation className='hide-on-sm'>
                 <Link to='/tech'>Tech</Link>
+                <Link to='/books'>Books</Link>
+                <Link to='/podcasts'>Podcasts</Link>
                 <Link to='/games'>Games</Link>
               </Navigation>
             </Header>
             <Drawer title='Product Hunt'>
               <Navigation>
                 <Link to='/tech' onClick={this.closeDrawer}>Tech</Link>
+                <Link to='/books' onClick={this.closeDrawer}>Books</Link>
+                <Link to='/podcasts' onClick={this.closeDrawer}>Podcasts</Link>
                 <Link to='/games' onClick={this.closeDrawer}>Games</Link>
               </Navigation>
             </Drawer>
             <Content>
               <Match exactly pattern='/' render={() => <Redirect to='/tech' />} />
-              <Match exactly pattern='/tech' component={Tech} />
-              <Match exactly pattern='/games' component={Games} />
+              <Match exactly pattern='/tech' component={() => <Posts category='tech' />} />
+              <Match exactly pattern='/books' component={() => <Posts category='books' />} />
+              <Match exactly pattern='/podcasts' component={() => <Posts category='podcasts' />} />
+              <Match exactly pattern='/games' component={() => <Posts category='games' />} />
             </Content>
           </Layout>
         </HashRouter>
--- src/Posts.js
+++ src/Posts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { string } from 'prop-types';
 import { Spinner, Card, CardTitle, CardText, CardActions, Button, Snackbar } from 'react-mdl';
 
 const styles = {
@@ -14,10 +15,27 @@ const styles = {
   card: {
     width: '512px',
     margin: 'auto'
+  },
+
+  noPostsWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    position: 'relative',
+    top: '200px'
+  },
+
+  noPosts: {
+    color: '#3F51B5',
+    fontSize: '2rem'
   }
 };
 
-class Tech extends Component {
+class Posts extends Component {
+  static propTypes = {
+    category: string.isRequired
+  }
+
   state = {
     posts: [],
     isLoaded: false,
@@ -29,14 +47,13 @@ class Tech extends Component {
     window.addEventListener('offline', this.showOfflinePopUp);
     window.addEventListener('online', this.showBackOnlinePopUp);
 
-    const url = 'https://api.producthunt.com/v1/categories/tech/posts';
+    const url = `https://api.producthunt.com/v1/categories/${this.props.category}/posts`;
     fetch(url, {
       method: 'get',
       headers: {
         Authorization: 'Bearer 6f541345132b797e0e11785199ff70a2db18bf7a6fe6f6f2aa650edd6bff8c1f'
       }
     }).then(response => response.json()).then(data => {
-      console.log(data);
       this.setState(() => {
         return {
           posts: data.posts,
@@ -94,19 +111,28 @@ class Tech extends Component {
         {
           !this.state.isLoaded && <Spinner style={styles.spinner} />
         }
-        {this.state.posts.map(post => (
-          <Card key={post.id} shadow={0} style={styles.card}>
-            <CardTitle style={{color: '#fff', height: '176px', background: `url(${post.thumbnail.image_url}) center / cover`}}>
-              {post.tagline}
-            </CardTitle>
-            <CardText>
-              Posted by {post.user.name}
-            </CardText>
-            <CardActions>
-              <Button colored>Show {post.comments_count} Comments</Button>
-            </CardActions>
-          </Card>
-        ))}
+        {
+          this.state.isLoaded && this.state.posts.length === 0 &&
+          <div style={styles.noPostsWrapper}>
+            <p style={styles.noPosts}>Sorry, there's been no new posts for that category today.</p>
+            <p style={styles.noPosts}>Please pick another category.</p>
+          </div>
+        }
+        {
+          this.state.posts.map(post => (
+            <Card key={post.id} shadow={0} style={styles.card}>
+              <CardTitle style={{color: '#fff', height: '176px', background: `url(${post.thumbnail.image_url}) center / cover`}}>
+                {post.tagline}
+              </CardTitle>
+              <CardText>
+                Posted by {post.user.name}
+              </CardText>
+              <CardActions>
+                <Button colored>Show {post.comments_count} Comments</Button>
+              </CardActions>
+            </Card>
+          ))
+        }
         <Snackbar active={this.state.isOffline} action='Undo' onTimeout={this.hideOfflinePopUp}>
           Now you are offline, but still your application works. Magic!
         </Snackbar>
@@ -118,4 +144,4 @@ class Tech extends Component {
   }
 }
 
-export default Tech;
+export default Posts;
